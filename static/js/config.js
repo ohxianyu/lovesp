3ee8160ae5172b1c71a85ff17b4d15862b758523
ohxianyu <selfbase@163.com>
2023-04-14 17:03:17 +0800

    Add files via upload

---
+++ static/js/config.js
@@ -0,0 +1,15 @@
+import config from '@/common/config.js';
+const  dev = 'weixin';
+const  appId = 0;
+
+const  apiUrl 	 = process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:81/' :'http://127.0.0.1:81/';
+
+const  staticUrl = staticUrl;
+
+
+export default {
+	appId,
+	dev,
+	apiUrl,
+	staticUrl,
+}
